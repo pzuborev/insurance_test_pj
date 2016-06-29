a34c112eff38762c7e6b348df8e3d94c9b3db690
pzuborev <pzuborev@gmail.com>
2016-06-29 21:36:56 +0300

    selected table

--- client/js/calculator.js
+++ client/js/calculator.js
@@ -4,45 +4,52 @@
 
     app.controller('CalcController', function ($scope) {
         // данные по рискам
-        $scope.risks = [
-            {
-                riskName: "Д",
-                forIndividualType: "Застрахованное лицо",
-                riskAmount: "2000",
-                payAmount: "500",
-                term: "10",
-                payTerm: "10",
-                nettoTariff: "0.0001"
-            },
-            {
-                riskName: "C",
-                forIndividualType: "Застрахованное лицо",
-                riskAmount: "2000",
-                payAmount: "500",
-                term: "10",
-                payTerm: "10",
-                nettoTariff: "0.0001"
-            },
-            {
-                riskName: "C(НС)",
-                forIndividualType: "Застрахованное лицо",
-                riskAmount: "200",
-                payAmount: "1",
-                term: "10",
-                payTerm: "10",
-                nettoTariff: "0.000133"
-            },
-            {
-                riskName: "C(ДТП)",
-                forIndividualType: "Застрахованное лицо",
-                riskAmount: "1000",
-                payAmount: "10",
-                term: "10",
-                payTerm: "10",
-                nettoTariff: "0.000221"
-            }
-        ];
-
+        $scope.risks = {
+            selectedRowNo: 1,
+            riskDataSet: [
+                {
+                    rowNo: "1",
+                    riskName: "Д",
+                    forIndividualType: "Застрахованное лицо",
+                    riskAmount: "2000",
+                    payAmount: "500",
+                    term: "10",
+                    payTerm: "10",
+                    nettoTariff: "0.0001"
+                },
+                {
+                    rowNo: "2",
+                    riskName: "C",
+                    forIndividualType: "Застрахованное лицо",
+                    riskAmount: "2000",
+                    payAmount: "500",
+                    term: "10",
+                    payTerm: "10",
+                    nettoTariff: "0.0001"
+                },
+                {
+                    rowNo: "3",
+                    riskName: "C(НС)",
+                    forIndividualType: "Застрахованное лицо",
+                    riskAmount: "200",
+                    payAmount: "1",
+                    term: "10",
+                    payTerm: "10",
+                    nettoTariff: "0.000133"
+                },
+                {
+                    rowNo: "4", riskName: "C(ДТП)",
+                    forIndividualType: "Застрахованное лицо",
+                    riskAmount: "1000",
+                    payAmount: "10",
+                    term: "10",
+                    payTerm: "10",
+                    nettoTariff: "0.000221"
+                }
+            ]};
+        $scope.setSelected = function (rowNo) {
+            $scope.risks.selectedRowNo = rowNo;
+        };
         // lookup списки
         // Программы страхования
         $scope.insSchemeList = [
@@ -91,7 +98,6 @@
         };
 
 
-
         // при изменении программы страхования, определяем программу страхования
         // todo: обавить фильтрацию списка
         $scope.InsSchemeChange = function () {
@@ -104,68 +110,68 @@
         };
 
     });
-   /* app.controller('InsSchemeController', InsSchemeController);
-    function InsSchemeController($timeout, $q, $log) {
-        var self = this;
-        self.simulateQuery = true;
-        self.isDisabled = false;
-        // list of `state` value/display objects
-        self.items = loadAll();
-        self.querySearch = querySearch;
-        self.selectedItemChange = selectedItemChange;
-        self.searchTextChange = searchTextChange;
-        //$log.info(loadAll());
-        // ******************************
-        // Internal methods
-        // ******************************
-        /!**
-         * Search for states... use $timeout to simulate
-         * remote dataservice call.
-         *!/
-        function querySearch(query) {
-            var results = query ? self.items.filter(createFilterFor(query)) : self.items,
-                deferred;
-            if (self.simulateQuery) {
-                deferred = $q.defer();
-                $timeout(function () {
-                    deferred.resolve(results);
-                }, Math.random() * 1000, false);
-                return deferred.promise;
-            } else {
-                return results;
-            }
-        }
+    /* app.controller('InsSchemeController', InsSchemeController);
+     function InsSchemeController($timeout, $q, $log) {
+     var self = this;
+     self.simulateQuery = true;
+     self.isDisabled = false;
+     // list of `state` value/display objects
+     self.items = loadAll();
+     self.querySearch = querySearch;
+     self.selectedItemChange = selectedItemChange;
+     self.searchTextChange = searchTextChange;
+     //$log.info(loadAll());
+     // ******************************
+     // Internal methods
+     // ******************************
+     /!**
+     * Search for states... use $timeout to simulate
+     * remote dataservice call.
+     *!/
+     function querySearch(query) {
+     var results = query ? self.items.filter(createFilterFor(query)) : self.items,
+     deferred;
+     if (self.simulateQuery) {
+     deferred = $q.defer();
+     $timeout(function () {
+     deferred.resolve(results);
+     }, Math.random() * 1000, false);
+     return deferred.promise;
+     } else {
+     return results;
+     }
+     }
 
-        function searchTextChange(text) {
-            $log.info('Text changed to ' + text);
-        }
+     function searchTextChange(text) {
+     $log.info('Text changed to ' + text);
+     }
 
-        function selectedItemChange(item) {
-            $log.info('Item changed to ' + JSON.stringify(item));
-        }
+     function selectedItemChange(item) {
+     $log.info('Item changed to ' + JSON.stringify(item));
+     }
 
-        /!**
-         * Build `states` list of key/value pairs
-         *!/
-        function loadAll() {
-            var allItems = '7 - "Лайф", 8 - "Пенсия", 9 - "Благосостояние"';
-            return allItems.split(/, +/g).map(function (item) {
-                return {
-                    value: item.toLowerCase(),
-                    display: item
-                };
-            });
-        }
+     /!**
+     * Build `states` list of key/value pairs
+     *!/
+     function loadAll() {
+     var allItems = '7 - "Лайф", 8 - "Пенсия", 9 - "Благосостояние"';
+     return allItems.split(/, +/g).map(function (item) {
+     return {
+     value: item.toLowerCase(),
+     display: item
+     };
+     });
+     }
 
-        /!**
-         * Create filter function for a query string
-         *!/
-        function createFilterFor(query) {
-            var lowercaseQuery = angular.lowercase(query);
-            return function filterFn(item) {
-                return (item.value.indexOf(lowercaseQuery) >= 0); /// === 0 точное совпадение
-            };
-        }
-    }*/
+     /!**
+     * Create filter function for a query string
+     *!/
+     function createFilterFor(query) {
+     var lowercaseQuery = angular.lowercase(query);
+     return function filterFn(item) {
+     return (item.value.indexOf(lowercaseQuery) >= 0); /// === 0 точное совпадение
+     };
+     }
+     }*/
 })();
 
