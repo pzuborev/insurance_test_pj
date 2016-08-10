be1e1c42fed9bb9db974169379640f555f00c32a
pzuborev <pzuborev@gmail.com>
2016-08-10 21:21:43 +0300

    fix nav bar

--- client/js/insuranceApp.js
+++ client/js/insuranceApp.js
@@ -53,3 +53,13 @@ app.controller('mainInsuranceCtrl',
             $scope.authorizedUserName = sessionHolder.getUserName();
         });
     });
+
+//pz test
+app.controller('DropdownCtrl', function($scope) {
+
+    $scope.items = [
+        "The first choice!",
+        "And another choice for you.",
+        "but wait! A third!"
+    ];
+});
